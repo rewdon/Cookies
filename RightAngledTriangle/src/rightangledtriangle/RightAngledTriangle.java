71cd13657fe71cb273cad7e6dec1641880c3ce48
rewdon <dev_rewdon@hotmail.com>
2015-12-17 15:00:52 +0000

    commit1

---
+++ RightAngledTriangle/src/rightangledtriangle/RightAngledTriangle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rightangledtriangle;
+
+/**
+ *
+ * @author Rewdon
+ */
+public class RightAngledTriangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+     NewJFrame theWindow = new NewJFrame();
+        //calling the form from the main method
+        theWindow.setVisible(true);
+    
+    
+    
+    }
+    
+}
